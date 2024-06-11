f5e52abbe951af77526a712b9dd3ce3d43516a8c
nick <nicholas.newberry@oxaro.com>
2024-06-11 14:11:48 -0400

    allow setting archyve public endpoint

--- src/lib/server/websearch/searchArchyve.ts
+++ src/lib/server/websearch/searchArchyve.ts
@@ -1,6 +1,7 @@
 import { ARCHYVE_API_KEY } from "$env/static/private";
 import { ARCHYVE_CLIENT_ID } from "$env/static/private";
 import { ARCHYVE_QUERY_URL } from "$env/static/private";
+import { ARCHYVE_PUBLIC_ENDPOINT } from "$env/static/private";
 
 export async function searchArchyve(query: string) {
 	const abortController = new AbortController();
@@ -35,7 +36,8 @@ export async function searchArchyve(query: string) {
 	}
 	const hits = jsonResponse.hits.slice(0, 10).sort((a, b) => a.distance - b.distance);
 
-	const favicon = new URL(query_url).origin + "/favicon.png";
+	const archyve_url = new URL(ARCHYVE_PUBLIC_ENDPOINT || query_url)
+	const favicon = archyve_url.origin + "/favicon.png";
 	const results: { link: string }[] = hits.map((hit) => {
 		return { link: hit.url, browserLink: hit.browser_url, favicon };
 	});
